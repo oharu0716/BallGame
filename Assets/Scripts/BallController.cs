c9f3176925f4ca5bbc74ea110e0e47719b5f81c1
oharu <nappo.sakura0716@gmail.com>
2025-09-29 19:14:03 +0900

    満足した

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallController : MonoBehaviour
 {
-    public GameObject ball;
+    public GameObject[] balls;
     public float baseWidth;
+    int count = 0;
     // Start is call
     // ed once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +16,7 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && count < 3)
         {
             Vector3 mousePosition = Input.mousePosition;
             // Zはカメラからの距離を指定（例：10ユニット先）
@@ -24,17 +26,37 @@ public class BallController : MonoBehaviour
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             Vector3 ballPosition = new Vector3(77f, 6.5f, worldPosition.z);
 
+            int r = Random.Range(0, 3);
+
             Instantiate(
-                ball,
-                ballPosition,
+                balls[r],
+                GetInstantiatePosition(),
                 Quaternion.identity
             );
+
+            count++;
+        }
+
+        if (count == 3)
+        {
+            StartCoroutine(DoAfterDelay());
         }
     }
 
     Vector3 GetInstantiatePosition()
     {
-        float z = baseWidth * (Input.mousePosition.z / Screen.width) - (baseWidth / 2);
-        return transform.position + new Vector3(77f, 6.5f, z);
+        float z = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        Debug.Log(z);
+        return new Vector3(77f, 6.5f, z);
+    }
+
+    System.Collections.IEnumerator DoAfterDelay()
+    {
+        yield return new WaitForSeconds(5f); // 10秒待つ
+        // 現在アクティブなシーンの名前を取得
+            string currentSceneName = SceneManager.GetActiveScene().name;
+
+            // そのシーンを再読み込み
+            SceneManager.LoadScene(currentSceneName);
     }
 }
